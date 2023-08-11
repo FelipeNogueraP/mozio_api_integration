c917cb959ecd18eb7cc7b53ae05d7580119240c2
FelipeNogueraP <felipenogueraprieto@hotmail.com>
2023-08-11 09:52:39 -0500

    poll_search method created

--- apiConsuming.py
+++ apiConsuming.py
@@ -1,6 +1,7 @@
 """The class and methods used for consuming the Mozio Api"""
 
 import requests
+import time
 
 
 class MozioApi:
@@ -18,6 +19,22 @@ class MozioApi:
         response = requests.post(endpoint, headers=cls.HEADERS, json=params)
         return response.json()
 
+    @classmethod
+    def poll_search(cls, search_id):
+        # Method to iterates in the results until "more_coming" became False
+        endpoint = f"{cls.BASE_URL}/v2/search/{search_id}/poll/"
+
+        while True:
+            response = requests.get(endpoint, headers=cls.HEADERS)
+            response_data = response.json()
+
+            # Pull until more_coming comes False
+            if not response_data.get('more_coming', True):
+                return response_data
+
+            # Waits two secconds before pull again
+            time.sleep(2)
+
 
 if __name__ == "__main__":
     search_params = {
@@ -32,4 +49,9 @@ if __name__ == "__main__":
 
     # Test Search method
     search_response = MozioApi.search(search_params)
-    print('search_response', search_response)
+    print('search_response: ', search_response)
+
+    # Test poll_search method
+    search_id = search_response['search_id']
+    poll_response = MozioApi.poll_search(search_id)
+    print('poll_response: ', poll_response)
